9e229f36bf2f306f5702cdd4b6fe2f4a960f785b
Abdelaal <ahmmohabd2001@gmail.com>
2023-12-23 19:41:25 +0200

    Keeping track of memory block locations

--- code/definitions.h
+++ code/definitions.h
@@ -1,6 +1,6 @@
 // Define a constant for the maximum number of processes
 #define MAX_PROCESSES 100
-#define MAX_MEM_SIZE 1024  // Assuming a total memory size of 1024 units
+#define MAX_MEM_SIZE 1024  // Assuming a total memory size of 1024 bytes
 
 // A special message type to indicate that all processes have been sent
 #define TERMINATION_MSG_TYPE 999
@@ -11,12 +11,13 @@ typedef short bool;
 
 // our stuff
 
-// Memory Blocks will be stored as a Doubly Linked List. This struct defines the structure of each block.
+// Blocks will be stored as a Doubly Linked List. This struct defines the structure of each block.
 // The Linked List will only store the nodes at the leaves of the tree; no further information is required to complete the algorithm.
 typedef struct Block {
     int size; // The maximum capacity of the block in Bytes
     int index; // The index of the block if the nearly complete binary tree was stored in an array, and indexed in the same way as a binary heap
     int isFree; // Indicates whether the block is occupied or not
+    int memoryLocation; // Indicates the first memory location occupied by the block
     struct Block* next; // Pointer to the next block
     struct Block* prev; // Pointer to the previous block
 } Block;
